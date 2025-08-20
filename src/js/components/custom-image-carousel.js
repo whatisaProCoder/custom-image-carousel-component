6c4ca24ea5d2b6634ed8e927dcac7cadd72c6a7c
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-21 02:07:42 +0530

    ddd

--- src/js/components/custom-image-carousel.js
+++ src/js/components/custom-image-carousel.js
@@ -53,7 +53,7 @@ export class CustomImageCarousel {
 
       if (items[index]) {
         items[index].scrollIntoView({
-          behavior: "smooth", inline: "center"
+          behavior: "smooth", inline: "center", block: "center"
         });
       }
     }, 1000);
