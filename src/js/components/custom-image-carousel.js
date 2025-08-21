9caaac24185e046e2a063fc9566eefdb5ecf1e27
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-21 13:48:35 +0530

    fix

--- src/js/components/custom-image-carousel.js
+++ src/js/components/custom-image-carousel.js
@@ -3,15 +3,21 @@ import leftArrowIcon from "../../icons/left_arrow_icon.svg";
 import rightArrowIcon from "../../icons/right_arrow_icon.svg";
 
 export class CustomImageCarousel {
+  #containerElement;
+  #navigationDotsContainer;
+  #navigationDots;
+  #imageGroupElement;
+  #currentIndex;
+  #arrayLength;
   constructor({ containerID, imageItemArray }) {
     this.containerID = containerID;
     this.imageItemArray = imageItemArray;
-    this.currentIndex = 0;
-    this.arrayLength = imageItemArray.length;
+    this.#currentIndex = 0;
+    this.#arrayLength = imageItemArray.length;
   }
   initialise() {
-    this.containerElement = document.querySelector(`#${this.containerID}`);
-    this.containerElement.innerHTML = /* html */ `
+    this.#containerElement = document.querySelector(`#${this.containerID}`);
+    this.#containerElement.innerHTML = /* html */ `
             <div class="mcic-container">
                 <div class="mcic-image-group">
                 </div>
@@ -26,19 +32,19 @@ export class CustomImageCarousel {
             </div>
         `;
 
-    this.navigationDotsContainer = this.containerElement.querySelector(
+    this.#navigationDotsContainer = this.#containerElement.querySelector(
       ".mcic-navigation-dots-container",
     );
-    for (let i = 0; i < this.arrayLength; i++) {
+    for (let i = 0; i < this.#arrayLength; i++) {
       const navigationDot = document.createElement("div");
       navigationDot.classList.add("mcic-navigation-dot");
-      this.navigationDotsContainer.appendChild(navigationDot);
+      this.#navigationDotsContainer.appendChild(navigationDot);
     }
 
-    this.navigationDots = this.containerElement.querySelectorAll(
+    this.#navigationDots = this.#containerElement.querySelectorAll(
       ".mcic-navigation-dot",
     );
-    this.#handleNavigationState(this.currentIndex);
+    this.#handleNavigationState(this.#currentIndex);
 
     this.#populateImageGroup();
     this.#preventUserScrolling();
@@ -47,7 +53,7 @@ export class CustomImageCarousel {
     return this;
   }
   #preventUserScrolling() {
-    this.imageGroupElement.addEventListener(
+    this.#imageGroupElement.addEventListener(
       "wheel",
       (event) => {
         event.preventDefault();
@@ -56,8 +62,8 @@ export class CustomImageCarousel {
     );
   }
   #populateImageGroup() {
-    this.imageGroupElement =
-      this.containerElement.querySelector(".mcic-image-group");
+    this.#imageGroupElement =
+      this.#containerElement.querySelector(".mcic-image-group");
 
     this.imageItemArray.forEach((imageItem) => {
       const imageElement = document.createElement("div");
@@ -68,12 +74,12 @@ export class CustomImageCarousel {
       image.style.scale = imageItem.scale;
 
       imageElement.appendChild(image);
-      this.imageGroupElement.appendChild(imageElement);
+      this.#imageGroupElement.appendChild(imageElement);
     });
   }
   #scrollToItem(index) {
     setTimeout(() => {
-      const items = this.containerElement.querySelectorAll(
+      const items = this.#containerElement.querySelectorAll(
         ".mcic-image-container",
       );
 
@@ -86,54 +92,54 @@ export class CustomImageCarousel {
     }, 100);
   }
   #handleUserActions() {
-    const leftButton = this.containerElement.querySelector(
+    const leftButton = this.#containerElement.querySelector(
       ".mcic-left-arrow-button",
     );
-    const rightButton = this.containerElement.querySelector(
+    const rightButton = this.#containerElement.querySelector(
       ".mcic-right-arrow-button",
     );
 
     leftButton.addEventListener("click", (event) => {
-      if (this.currentIndex > 0) {
-        this.#scrollToItem(--this.currentIndex);
-        this.#handleNavigationState(this.currentIndex);
+      if (this.#currentIndex > 0) {
+        this.#scrollToItem(--this.#currentIndex);
+        this.#handleNavigationState(this.#currentIndex);
       }
-      console.log(event.target, this.currentIndex);
+      console.log(event.target, this.#currentIndex);
     });
 
     rightButton.addEventListener("click", (event) => {
-      if (this.currentIndex < this.arrayLength - 1) {
-        this.#scrollToItem(++this.currentIndex);
-        this.#handleNavigationState(this.currentIndex);
+      if (this.#currentIndex < this.#arrayLength - 1) {
+        this.#scrollToItem(++this.#currentIndex);
+        this.#handleNavigationState(this.#currentIndex);
       }
-      console.log(event.target, this.currentIndex);
+      console.log(event.target, this.#currentIndex);
     });
 
-    this.navigationDots.forEach((dot, index) => {
+    this.#navigationDots.forEach((dot, index) => {
       dot.addEventListener("click", () => {
-        this.currentIndex = index;
-        this.#scrollToItem(this.currentIndex);
-        this.#handleNavigationState(this.currentIndex);
+        this.#currentIndex = index;
+        this.#scrollToItem(this.#currentIndex);
+        this.#handleNavigationState(this.#currentIndex);
       });
     });
   }
   #removeSelectedClassInNavigationDots() {
-    this.navigationDots.forEach((dot) => {
+    this.#navigationDots.forEach((dot) => {
       dot.classList.remove("mcic-navigation-dot-selected");
     });
   }
   #handleNavigationState(index) {
-    const leftButton = this.containerElement.querySelector(
+    const leftButton = this.#containerElement.querySelector(
       ".mcic-left-arrow-button",
     );
-    const rightButton = this.containerElement.querySelector(
+    const rightButton = this.#containerElement.querySelector(
       ".mcic-right-arrow-button",
     );
     this.#removeSelectedClassInNavigationDots();
-    this.navigationDots[index].classList.add("mcic-navigation-dot-selected");
+    this.#navigationDots[index].classList.add("mcic-navigation-dot-selected");
     if (index == 0) {
       leftButton.classList.add("mcic-arrow-button-hide");
-    } else if (index == this.arrayLength - 1) {
+    } else if (index == this.#arrayLength - 1) {
       rightButton.classList.add("mcic-arrow-button-hide");
     } else {
       leftButton.classList.remove("mcic-arrow-button-hide");
@@ -141,11 +147,13 @@ export class CustomImageCarousel {
     }
   }
   displayItemNumber(itemNumber) {
-    if (itemNumber >= 1 && itemNumber < this.arrayLength) {
-      this.currentIndex = itemNumber;
-      this.#handleNavigationState(this.currentIndex);
-      this.#scrollToItem(this.currentIndex);
-    }
+    setTimeout(() => {
+      if (itemNumber >= 1 && itemNumber < this.#arrayLength) {
+        this.#currentIndex = itemNumber;
+        this.#handleNavigationState(this.#currentIndex);
+        this.#scrollToItem(this.#currentIndex);
+      }
+    }, 750);
   }
 }
 
